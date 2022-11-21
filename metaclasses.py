2a91bf830d914017b75b41aee1757a826bd25a5a
liya-gafurova <gali4086555@gmail.com>
2022-11-21 18:05:42 +0600

    decorators and metaclasses

--- metaclasses.py
+++ metaclasses.py
@@ -1,64 +1,141 @@
-# Metaprogramming/SingletonDecorator.py
+import datetime
+import random
+from math import sqrt
+from typing import Optional
 
-def singleton(klass):
-    "Simple replacement of object creation operation"
-    def getinstance(*args, **kw):
-        if not hasattr(klass, 'instance'):
-            klass.instance = klass(*args, **kw)
-        return klass.instance
-    return getinstance
-
-def singleton(klass):
-    """
-    More powerful approach: Change the behavior
-    of the instances AND the class object.
-    """
-    class Decorated(klass):
-        def __init__(self, *args, **kwargs):
-            if hasattr(klass, '__init__'):
-                klass.__init__(self, *args, **kwargs)
-        def __repr__(self) : return klass.__name__ + " obj"
-        __str__ = __repr__
-    Decorated.__name__ = klass.__name__
-    class ClassObject:
-        def __init__(cls):
-            cls.instance = None
-        def __repr__(cls):
-            return klass.__name__
-        __str__ = __repr__
-        def __call__(cls, *args, **kwargs):
-            print( str(cls) + " __call__ ")
-            if not cls.instance:
-                cls.instance = Decorated(*args, **kwargs)
-            return cls.instance
-    return ClassObject()
-
-@singleton
-class ASingleton: pass
-
-a = ASingleton()
-b = ASingleton()
-print(a, b)
-print (a.__class__.__name__)
-print (ASingleton)
-assert a is b
-
-@singleton
-class BSingleton:
-    def __init__(self, x):
-        self.x = x
-
-c = BSingleton(11)
-d = BSingleton(22)
-assert c is d
-assert c is not a
-
-""" Output:
-ASingleton __call__
-ASingleton __call__
-(ASingleton obj, ASingleton obj)
-ASingleton
-ASingleton
-BSingleton __call__
-BSingleton __call__
 """
+https://www.youtube.com/watch?v=CukrRuOAVqg 
+https://www.youtube.com/watch?v=Rm4JP7JfsKY 
+
+"""
+
+
+###########################################################################
+#                                SINGLETON                                #
+###########################################################################
+
+
+class Singleton:
+    __instance = None
+
+    def __new__(cls, *args, **kwargs):
+        if cls.__instance is None:
+            cls.__instance = super(Singleton, cls).__new__(cls)
+
+        return cls.__instance
+
+
+class Logger(Singleton):
+    def write(self, msg: str):
+        print(f'Logger. {datetime.datetime.now()}: {msg}')
+
+
+log = Logger()
+log2 = Logger()
+
+print(log)
+print(log2)
+
+
+###########################################################################
+#                        SINGLETON WITH METACLASS                         #
+###########################################################################
+
+
+class SingletonMetaclass(type):
+    _instance = None
+
+    def __call__(cls, *args, **kwargs):
+        if cls._instance is None:
+            cls._instance = super(SingletonMetaclass, cls).__call__(*args, **kwargs)
+
+        return cls._instance
+
+
+class Base(object, metaclass=SingletonMetaclass):
+    def __new__(cls, *args, **kwargs):
+        print(f'{cls.__name__}: new')
+        return super(Base, cls).__new__(cls)
+
+    def __init__(self, *args, **kwargs):
+        print(f'{self.__class__.__name__}: init')
+
+    def __call__(self, *args, **kwargs):
+        print(f'{self.__class__.__name__}: call')
+
+
+class Custom(Base):
+    pass
+
+
+c = Custom(value='some_val')
+d = Custom(value='other val')
+
+print(d)
+print(c)
+
+
+###########################################################################
+#                        SINGLETON WITH DECORATORS                        #
+###########################################################################
+
+
+##################################################
+#                 function logic                 #
+##################################################
+
+_sqrt_cache = {}
+
+
+def get_sqrt_with_cache(a: int):
+    if a not in _sqrt_cache:
+        print('compute new')
+        _sqrt_cache[a] = sqrt(a)
+
+    return _sqrt_cache[a]
+
+
+# while True:
+#     number = input('Enter number: ')
+#     result = get_sqrt_with_cache(int(number))
+#     print(f'SQRT is: {result}')
+
+
+##################################################
+#            function with decorators            #
+##################################################
+
+def cached(func: callable):
+    _cache = {}
+
+    def new_func(a):
+        if a not in _cache:
+            _cache[a] = func(a)
+        return _cache[a]
+
+    return new_func
+
+
+@cached
+def get_sqrt(a: int) -> float:
+    print('Compute SQRT value...')
+    return sqrt(a)
+
+
+@cached
+def get_pow(a: int) -> float:
+    print('Compute POW value...')
+    return pow(a, 2)
+
+
+funcs = [get_pow, get_sqrt]
+while True:
+    number = input('Enter number: ')
+    func = random.choice(funcs)
+    result = func(int(number))
+    print(f'Result is: {result}\n\n')
+
+
+##################################################
+#             decorators with params             #
+##################################################
---
+++ utils.py
@@ -0,0 +1,35 @@
+import argparse
+
+
+def get_middle_line(comment:str, header_len: int):
+    comment_len = len(comment)
+    spaces_len = header_len - 2 - comment_len
+    if spaces_len > 0:
+        spaces_left = spaces_len // 2
+        spaces_right = spaces_len - spaces_left
+        return '#' + ' ' * spaces_left + comment + ' ' * spaces_right + '#'
+
+
+def generate_comment_header(comment: str, header_type: str = 'large') -> str:
+    if header_type == 'large':
+        HEADER_LEN = 75
+        case_func = str.upper
+    elif header_type == 'medium':
+        HEADER_LEN = 50
+        case_func = str.lower
+
+    else:
+        raise NotImplementedError
+
+    first = '#' * HEADER_LEN
+    comment_line = case_func(comment)
+    middle = get_middle_line(comment_line, HEADER_LEN)
+
+    header = f'{first}\n{middle}\n{first}\n\n\n'
+    print(header)
+
+    return header
+
+
+generate_comment_header('decorators with params', header_type='medium')
+
